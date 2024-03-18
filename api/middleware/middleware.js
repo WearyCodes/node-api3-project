f94f4c0c30754ac2459c2bea36367bdc64c2be2e
Samuel Sanchez <wearycodes@gmail.com>
2024-03-17 21:43:22 -0500

    Commiting because middleware is connected

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,49 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log('method: ' + req.method, 'URL: ' + req.url, 'time: ' + new Date().toTimeString().split(" ")[0]);
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+ try { const user = await User.getById(req.params.id)
+  if (!user) {
+    res.status(404).json({
+      message: 'User does not exist'
+    })
+  } else {
+    req.user = user
+    next()
+  }}
+catch (err){
+  res.status(500).json({
+    message: 'problem finding user'
+  })
+}
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+ console.log(req.user)
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if (!req.body.name) {
+    res.status(400).json({
+      message: 'missing required text field'
+    })
+  } else {
+    console.log('validate POST is OK!')
+  }
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
